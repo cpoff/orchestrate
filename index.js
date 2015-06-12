6276ed74675005d451d4a801c74d72940de968f6
cpoff <curt.poff@gmail.com>
2015-06-12 13:37:19 -0700

    First pass at index

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 var fs = require('fs'); 
-var config = require('./config.js');
+var config = require('./config.js');<div class="input-group">
+  <span class="input-group-addon">$</span>
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+  <span class="input-group-addon">.00</span>
+</div>
 var db = require('orchestrate')(config.dbKey);
 
 //fs.readFile('config.json', function(error, contents) {
 //   config = JSON.parse(contents); 
 
-db.put('collection', 'key', {
-  "name": "Unkle Kracker",
-  "hometown": "Portland, OR",
-  "twitter": "@yomamma"
-})
-.then(function (res) {
-  console.log(res.statusCode);
-})
-.fail(function (err) {});
+//db.put('collection', 'key', {
+//  "name": "Unkle Kracker",
+//  "hometown": "Portland, OR",
+//  "twitter": "@yomamma"
+//})
+//.then(function (res) {
+//  console.log(res.statusCode);
+//})
+//.fail(function (err) {});
 
 db.list('jsi-test').then(function(res) {
     console.log(res.body.results);
 }).fail(function(err) {});
 
 
-
-
-for (i = 0; i < items.length; i++) {
-   console.log(items[i]); 
-}
